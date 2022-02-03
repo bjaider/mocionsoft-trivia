81f29afce1044894a8cabde7e6b6e3c241e5ce41
bjaider <bjaider@uninorte.edu.co>
2022-02-03 14:23:44 -0500

    feat: become question score  from static to dynamic

--- src/Components/QuestionScore/QuestionScore.js
+++ src/Components/QuestionScore/QuestionScore.js
@@ -2,23 +2,35 @@ import React, {useState} from 'react'
 import Button from '../Button/Button'
 import Modal from '../Modal/Modal'
 import './QuestionScore.scss'
-const QuestionScore = () => {
+const QuestionScore = ({question, correct, index}) => {
   const [showModal, setShowModal] = useState(false)
 
   return (
     <div className="question-score-container">
       <Modal show={showModal} setShowModal={setShowModal}>
-        <p>Modal</p>
+        <p>{question}</p>
       </Modal>
-      <p>1</p>
-      <p onClick={() => setShowModal(true)}>Question </p>
-      <Button className={'close-button'}>
-        <img
-          src="https://res.cloudinary.com/dhxg3zyjz/image/upload/v1643870057/MocionSoft/close_1_m5c4mx.png"
-          width={42}
-          alt="Close Button"
-        />
-      </Button>
+      <p className="question-index">{index}</p>
+      <p className="show-question" onClick={() => setShowModal(true)}>
+        Show Question
+      </p>
+      {correct ? (
+        <Button className={'close-button'}>
+          <img
+            src="https://res.cloudinary.com/dhxg3zyjz/image/upload/v1643911011/MocionSoft/check_1_umstzl.png"
+            width={32}
+            alt="Close Button"
+          />
+        </Button>
+      ) : (
+        <Button className={'close-button'}>
+          <img
+            src="https://res.cloudinary.com/dhxg3zyjz/image/upload/v1643911007/MocionSoft/wrong_1_ncrpnt.png"
+            width={32}
+            alt="Close Button"
+          />
+        </Button>
+      )}
     </div>
   )
 }
