82a561f50b77347d0155ad363abc38d15841ffd8
bjaider <bjaider@uninorte.edu.co>
2022-02-03 01:52:10 -0500

    fear: add styles to top component

--- src/Views/Trivia/Trivia.js
+++ src/Views/Trivia/Trivia.js
@@ -4,10 +4,12 @@ import Question from '../../Components/Question/Question'
 import useGetQuestions from '../../Hooks/useGetQuestions'
 import Button from '../../Components/Button/Button'
 import './Trivia.scss'
+import Countdown from '../../Components/Countdown/Countdown'
 const Trivia = () => {
   const {questionsData} = useGetQuestions()
-  const [question, setQuestion] = useState(1)
+  const [question, setQuestion] = useState(0)
   const [questionNumber, setQuestionNumber] = useState(1)
+  
   useEffect(() => {
     if (questionsData) {
       setQuestion(questionsData[questionNumber])
@@ -16,6 +18,16 @@ const Trivia = () => {
 
   return (
     <div className="trivia-container">
+      <div className="trivia-top">
+        <Button className={'close-button'}>
+          <img
+            src="https://res.cloudinary.com/dhxg3zyjz/image/upload/v1643870057/MocionSoft/close_1_m5c4mx.png"
+            width={42}
+            alt="Close Button"
+          />{' '}
+        </Button>
+        <Countdown time={10} />
+      </div>
       <CategoryImage
         src={
           'https://res.cloudinary.com/dhxg3zyjz/image/upload/v1643863251/MocionSoft/sports_1_dwndn4.png'
@@ -27,9 +39,19 @@ const Trivia = () => {
         questionNumber={questionNumber}
         questionsTotal={10}
       />
-      <div className='answer-buttons'>
-        <Button className="answer-button">True</Button>
-        <Button className="answer-button">False</Button>
+      <div className="answer-buttons">
+        <Button
+          className="answer-button"
+          onClick={() => setQuestionNumber(questionNumber + 1)}
+        >
+          True
+        </Button>
+        <Button
+          className="answer-button"
+          onClick={() => setQuestionNumber(questionNumber + 1)}
+        >
+          False
+        </Button>
       </div>
     </div>
   )
